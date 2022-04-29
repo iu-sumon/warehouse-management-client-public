633078c633d3a6f914daec24b281163924ab9750
Md. Sumon Mia <md.sumonmia.cse.iu@gmail.com>
2022-04-29 21:19:02 +0600

    banner and carousel added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactCarousel from '../ReactCarousel/ReactCarousel';
 import bPhoto from '../../images/banner/banner1_1_400x320.webp'
-
+import './Home.css'
 const Home = () => {
     return (
-        <div style={{ backgroundColor: "#2DBEBF" }} className='py-5 h-100'>
+        <div className='py-5 h-100 color2'>
 
-            <div className='d-flex justify-content-evenly align-items-center border border-1'>  
+            <div className='d-flex justify-content-evenly align-items-center '>  
                 <div>
-                    <img className='rounded-3 shadow border border-1 p-1' src={bPhoto} alt="" />
+                    <img className='rounded-3 shadow  p-1' src={bPhoto} alt="" />
                 </div>
-                <div className='w-50 border border-1'>
+                <div className='w-50'>
                     <ReactCarousel></ReactCarousel>
                 </div>
             </div>
